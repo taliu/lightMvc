52884b7762ffd8a1df4b00265f59216eb5824427
lranye <1036499685@qq.com>
2015-08-27 22:17:40 +0800

    add RouteModule

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var connact=require("./lib/connact");
 http.createServer(function(req,res){
 	var ct=connact(req,res);
 	ct.use(require("./lib/RequestDataModule"));
+	ct.use(require("./lib/StaticFileModule"));
 	ct.use(function(err,req,res,next){
 		console.log("query",req.query);
 		console.log("url data",req.urlData);
@@ -11,7 +12,6 @@ http.createServer(function(req,res){
 		res.end("hello world");
 		next();
 	});
-	ct.use(require("./lib/StaticFileModule"));
 	ct.done();
 }).listen(5566,function(){
 	console.log("web server listen at port 5566");
